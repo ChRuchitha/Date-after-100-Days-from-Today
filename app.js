476bac6647646887674d33821a26cd531d489f8c
root <you@example.com>
2024-09-05 15:08:21 +0000

    commit message

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+
+app.get("/", (request, response) => {
+  let date = new Date();
+  const result = addDays(
+    new Date(date.getFullYear(), date.getMonth(), date.getDate()),
+    100
+  );
+  response.send(
+    `${result.getDate()}/${result.getMonth() + 1}/${result.getFullYear()}`
+  );
+  console.log(date);
+});
+
+app.listen(3000);
+module.exports = app;
